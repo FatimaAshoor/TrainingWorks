4bec71ec67d1c3f27bbd7b4e366922a047717b64
Fatima Ashoor <1fashoor@example.com>
2024-08-20 08:12:36 +0300

    complated calculator app

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,5 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
 
 void main() {
@@ -7,119 +9,209 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'Flutter Demo',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a purple toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: MyHomePage(),
     );
   }
 }
 
 class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
+  const MyHomePage({super.key});
 
   @override
+
+
   State<MyHomePage> createState() => _MyHomePageState();
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
 
-  void _incrementCounter() {
+  String result ='0';
+  String finalResult ='0';
+  double num1 = 0;
+  double num2 = 0;
+  String operation='';
+
+  checkPressedButton(String value){
+    if(value == "C"){
+      finalResult = '0';
+      num1 = 0;
+      num2 = 0;
+      operation = '';
+    }
+    else if (value == "+" || value == "-" || value =="x" || value == "/"){
+      num1 = double.parse(result);
+      operation = value;
+      finalResult = "0";
+      result = result+value;
+    }
+    else if(value=="."){
+      if(finalResult.contains(".")){}
+      else{
+        finalResult = finalResult+value;
+      }
+
+    }
+    else if(value=="+/-"){
+      if(result.toString().contains('-')){
+        result.toString().substring(1);
+        finalResult = result;
+      } else {
+        result = '-'+result;
+        finalResult = result;
+      }
+    }
+    else if(value=="%"){
+      num2 = double.parse(result);
+      finalResult = (num2/100).toString();
+    }
+    else if(value=="="){
+      num2 = double.parse(result);
+      if(operation=='+'){
+        finalResult = (num1+num2).toString();
+      }
+      if(operation=='-'){
+        finalResult = (num1-num2).toString();
+      }
+      if(operation=='x'){
+        finalResult = (num1*num2).toString();
+      }
+      if(operation=='/'){
+        finalResult = (num1/num2).toString();
+      }
+    }
+    else {
+      finalResult = finalResult + value;
+    }
     setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
+      result = double.parse(finalResult).toString();
     });
   }
 
-  @override
-  Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
+
+  Widget calcButton(String btnText, Color btnColor, Color textColor){
+    return Container(
+      decoration: BoxDecoration(borderRadius: BorderRadius.circular(10)),
+      child: MaterialButton(
+        onPressed: (){
+          checkPressedButton(btnText);
+        },
+        child: Text("$btnText", style: TextStyle(fontSize: 35, color: textColor),),
+        color: btnColor,
+        padding: EdgeInsets.all(20),
       ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
+
+
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.black26,
+      body: Padding(
+        padding: EdgeInsets.symmetric(horizontal: 5),
+        child: SingleChildScrollView(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.end,
+            children: [
+              SingleChildScrollView(
+                scrollDirection: Axis.vertical,
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.end,
+                  children: [
+                    Padding(padding: EdgeInsets.all(10),
+                    child: Text("$finalResult", textAlign: TextAlign.left, style: TextStyle(color: Colors.white, fontSize: 100),),
+                    )
+                  ],
+                ),
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  calcButton("C", Colors.grey, Colors.black),
+                  calcButton("+/-", Colors.grey, Colors.black),
+                  calcButton("%", Colors.grey, Colors.black),
+                  calcButton("÷", Colors.orange, Colors.white),
+                ],
+              ),
+              SizedBox(height:10,),
+          
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  calcButton("7", Colors.white10, Colors.white),
+                  calcButton("8", Colors.white10, Colors.white),
+                  calcButton("9", Colors.white10, Colors.white),
+                  calcButton("x", Colors.orange, Colors.white),
+                ],
+              ),
+              SizedBox(height:10,),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  calcButton("4", Colors.white10, Colors.white),
+                  calcButton("5", Colors.white10, Colors.white),
+                  calcButton("6", Colors.white10, Colors.white),
+                  calcButton("-", Colors.orange, Colors.white),
+                ],
+              ),
+              SizedBox(height:10,),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  calcButton("1", Colors.white10, Colors.white),
+                  calcButton("2", Colors.white10, Colors.white),
+                  calcButton("3", Colors.white10, Colors.white),
+                  calcButton("+", Colors.orange, Colors.white),
+                ],
+              ),
+              SizedBox(height:10,),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Container(
+                    decoration: BoxDecoration(borderRadius: BorderRadius.circular(10)),
+                    child: MaterialButton(
+                        onPressed: (){
+                          setState(() {
+                            checkPressedButton("0");
+                          });
+                        },
+                      padding: EdgeInsets.fromLTRB(34, 20, 128, 20),
+                      child: Text("0 ", style: TextStyle(fontSize: 35, color: Colors.white),),
+                      color: Colors.white10,
+                    ),
+                  ),
+                  calcButton(",", Colors.white10, Colors.white),
+                  calcButton("=", Colors.orange, Colors.white),
+                ],
+              ),
+              SizedBox(height:10,),
+            ],
+          ),
+        ),
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        backgroundColor: Colors.black26,
+        selectedItemColor: Colors.orange,
+        unselectedItemColor: Colors.grey,
+        items: [
+          BottomNavigationBarItem(icon: Icon(Icons.calculate_outlined), label: "Edit"),
+          BottomNavigationBarItem(icon: Icon(Icons.camera_alt_outlined), label: "Edit"),
+          BottomNavigationBarItem(icon: Icon(Icons.keyboard_voice_outlined), label: "Edit"),
+        ],
+      ),
+    );
+  }
+
+
 }
