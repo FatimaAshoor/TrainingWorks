8cb53e6c295b15a15621c8840487bb65e5434f1a
Fatima Ashoor <1fashoor@example.com>
2024-08-29 13:04:09 +0300

    added rest operations to service

--- lib/service/post_service.dart
+++ lib/service/post_service.dart
@@ -1,7 +1,5 @@
 import 'dart:convert';
-
 import 'package:http/http.dart';
-
 import 'package:training_works/db/sql_db.dart';
 import 'package:training_works/model/post_model.dart';
 
@@ -25,7 +23,59 @@ class PostService{
     else{
       return "ERROR";
     }
+  }
 
+
+  Future addData(int id, String title, String body ) async{
+    try{
+      var res = await post(Uri.parse("https://dummyjson.com/posts"), body: {
+        jsonEncode({
+          'id' : id.toString(),
+          'title' : title,
+          'body' : body
+        })
+      });
+
+      if(res.statusCode == 201){
+        return PostModel.fromJson(jsonDecode(res.body));
+      }
+    }
+    catch(e){
+      print("Error :\n $e");
+    }
+  }
+
+
+  Future updateData(int id, String title, String body ) async{
+    try{
+      var res = await put(Uri.parse("https://dummyjson.com/posts/$id"), body: {
+        jsonEncode({
+          'id' : id.toString(),
+          'title' : title,
+          'body' : body
+        })
+      });
+
+      if(res.statusCode ==200){
+        return PostModel.fromJson(jsonDecode(res.body));
+      }
+    }
+    catch(e){
+      print("Error :\n $e");
+    }
+  }
+
+
+  Future deleteData(int id) async{
+    var res = await delete(Uri.parse("https://dummyjson.com/posts/$id"));
+
+
+      if(res.statusCode == 201 || res.statusCode ==200){
+        return PostModel.fromJson(jsonDecode(res.body));
+      }
+      else{
+        print("ERROR");
+      }
   }
 
   Future<List<Map>> showPosts() async {
