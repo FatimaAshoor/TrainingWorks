6742575bf64fbe709a1fe0c7b4fe5d83512b26eb
FatimaAshoor <1fashoor@gmail.com>
2024-09-18 17:47:46 +0300

    I update colors file

--- lib/constant/colors.dart
+++ lib/constant/colors.dart
@@ -1,13 +1,11 @@
 import 'dart:ui';
 
 class NotesColor{
-  static Color orange = Color(0xffFFA447);
-  static Color blue = Color(0xff7ECBFF);
-  static Color white = Color(0xffFFFFFF);
-  static Color black = Color(0xff080808);
-
+  static Color orange = const Color(0xffFFA447);
+  static Color green = const Color(0xffB5E6DB);
+  static Color white = const Color(0xffFFFFFF);
+  static Color black = const Color(0xff080808);
   static Color back = const Color(0xff100F11);
   static Color grey = const Color(0xff171717);
   static Color lightGrey = const Color(0xffb1b1b1);
-  static Color green = const Color(0xffB5E6DB);
 }
